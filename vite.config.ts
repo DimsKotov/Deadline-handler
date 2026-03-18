3d6177845dde4c13e592288cf61504c49f2d76de
Dima Kotov <DimsiKotov@yandex.ru>
2026-03-18 15:48:42 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages для репозитория DimsKotov/Deadline-handler:
+  // https://dimskotov.github.io/Deadline-handler/
+  base: "/Deadline-handler/",
   plugins: [
     react({
       babel: {
