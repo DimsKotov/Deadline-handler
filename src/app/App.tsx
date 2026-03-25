8dfa2010a744e26ba58c95fbe5caa0ae467deac4
Dima Kotov <DimsiKotov@yandex.ru>
2026-03-25 17:29:53 +0300

    Initial project upload

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,17 @@ import "./App.css";
 import Header from "../app/layout/Header/Header";
 import MainLayout from "../app/layout/MainLayout/MainLayout";
 import Footer from "../app/layout/Footer/Footer";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <div className="appShell">
         <Header />
         <MainLayout />
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
